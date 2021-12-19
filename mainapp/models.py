a71ebc4918a713ea5a7c467351ba36b32cb3f906
Sebkd <sebkd@mail.ru>
2021-12-19 22:51:19 +0300

    add database

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+
 # Create your models here.
