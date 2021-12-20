6be14f0c2c5767cbcc97185511ea337db61fad8b
Sebkd <sebkd@mail.ru>
2021-12-20 23:30:49 +0300

    only that lesson3

--- myShop/views.py
+++ myShop/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
-
+from mainapp.models import Product
 
 
 def index(request):
     title = 'myShop'
+    products = Product.objects.all()[:4]
     links_menu = ['домой', 'продукты', 'контакты',]
     context_page = {
         'title': title,
         'links_menu': links_menu,
+        'products': products,
     }
     return render(request, 'myShop/index.html', context=context_page)
 
