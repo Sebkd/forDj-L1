55d83f2d7c4e0dd53dad9ae15a36815df56da48b
Sebkd <sebkd@mail.ru>
2021-12-20 21:55:11 +0300

    paused

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from mainapp.models import ProductCategory
+from mainapp.models import ProductCategory, Product
 
 admin.site.register(ProductCategory)
+admin.site.register(Product)
 
 # Register your models here.
