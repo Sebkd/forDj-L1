4696b72c791e1546dbcdd5d3fa502242e3e3dd10
Sebkd <sebkd@mail.ru>
2022-01-07 21:26:45 +0300

    code L5

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 
-from mainapp.models import Product
+from mainapp.models import Product, ProductCategory
 
 
 def products(request, pk=None):
     title = 'каталог'
     links_menu = ['домой', 'продукты', 'контакты',]
-    same_products = [
-    {'href': 'products_all', 'name': 'все'},
-    {'href': 'products_home', 'name': 'дом'},
-    {'href': 'products_office', 'name': 'офис'},
-    {'href': 'products_modern', 'name': 'модерн'},
-    {'href': 'products_classic', 'name': 'классика'},
-    ]
+    same_products = ProductCategory.objects.all()
+    #     [
+    # {'href': 'products_all', 'name': 'все'},
+    # {'href': 'products_home', 'name': 'дом'},
+    # {'href': 'products_office', 'name': 'офис'},
+    # {'href': 'products_modern', 'name': 'модерн'},
+    # {'href': 'products_classic', 'name': 'классика'},
+    # ]
    # product = Product.objects.get(id=pk)
     context_page = {
         'title': title,
