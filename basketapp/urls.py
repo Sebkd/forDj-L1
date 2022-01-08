7915280b89121d6979865e5bd92d749e9cc34dda
Sebkd <sebkd@mail.ru>
2022-01-08 12:18:33 +0300

    create basketapp
    add import in url of basketapp

---
+++ basketapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+# from .views import products
+
+import basketapp.views as basketapp
+
+app_name = 'basketapp'
+
+urlpatterns = [
+    
+]
+
+# urlpatterns = [
+#     path('', products, name='index'),
+#     path('category/<int:pk>/', products, name='category'),
+# ]
