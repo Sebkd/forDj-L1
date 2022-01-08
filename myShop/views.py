ecc528eb885f618ca115a2857b19a7c56dc4d6ab
Sebkd <sebkd@mail.ru>
2022-01-08 18:10:19 +0300

    pause

--- myShop/views.py
+++ myShop/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+from basketapp.models import Basket
 from mainapp.models import Product, ProductCategory
 
 
 def index(request):
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter (user = request.user)
     title = 'myShop'
     products = Product.objects.all()[:4]
     links_menu = ['домой', 'продукты', 'контакты',]
@@ -11,15 +15,20 @@ def index(request):
         'title': title,
         'links_menu': links_menu,
         'products': products,
+        'basket': basket,
     }
     return render(request, 'myShop/index.html', context=context_page)
 
 
 def contacts(request):
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter (user = request.user)
     title = 'контакты'
     links_menu = ['домой', 'продукты', 'контакты',]
     context_page = {
         'title': title,
         'links_menu': links_menu,
+        'basket': basket,
     }
     return render(request, 'myShop/contacts.html', context=context_page)
