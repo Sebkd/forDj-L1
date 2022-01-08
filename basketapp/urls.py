04e399e2972eb373c2f90eacd258775866da28bf
Sebkd <sebkd@mail.ru>
2022-01-08 12:48:00 +0300

    code basket
    add basket.html

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -7,7 +7,9 @@ import basketapp.views as basketapp
 app_name = 'basketapp'
 
 urlpatterns = [
-    
+    path('', basketapp.basket, name='index'),
+    path('add/<int:pk>/', basketapp.basket_add, name='add'),
+    path('remove/<int:pk>/', basketapp.basket_remove, name='remove'),
 ]
 
 # urlpatterns = [
--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
+def basket(request):
+    context = {}
+    return render(request, 'basketapp/basket.html', context)
+
+def basket_add(request):
+    pass
+
+def basket_remove(request):
+    pass
+
 # Create your views here.
