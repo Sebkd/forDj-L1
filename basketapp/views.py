b246f0b68a622f9a48f863e02c9cba2910902f5d
Sebkd <sebkd@mail.ru>
2022-01-09 20:39:30 +0300

    end L-6

--- basketapp/views.py
+++ basketapp/views.py
@@ -49,7 +49,7 @@ def basket_remove(request, pk):
 
 @login_required
 def basket_edit(request, pk, quantity):
-    if request.is_ajax():
+    if (request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'):
         quantity = int(quantity)
         new_basket_item = Basket.objects.get(pk=int(pk))
 
