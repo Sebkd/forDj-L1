ed5588aba6619ba2dcbf0d18417480abf68dbffc
Sebkd <sebkd@mail.ru>
2022-01-09 17:45:50 +0300

    created login form
    created login user that add witout unauth use

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from basketapp.models import Basket
 from mainapp.models import Product
 from myShop.views import get_basket
 
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def basket(request):
     basket = get_basket (user = request.user)
     basket_items = basket.order_by('product__category')
@@ -19,7 +22,10 @@ def basket(request):
     }
     return render(request, 'basketapp/basket.html', context = context_page)
 
+@login_required
 def basket_add(request, pk):
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product_detail', args=[pk]))
     product = get_object_or_404(Product, pk=pk)
 
     basket = Basket.objects.filter(user = request.user, product = product).first() #корзинок может
@@ -33,6 +39,7 @@ def basket_add(request, pk):
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+@login_required
 def basket_remove(request, pk):
     basket_record = get_object_or_404(Basket, pk=pk)
     basket_record.delete()
