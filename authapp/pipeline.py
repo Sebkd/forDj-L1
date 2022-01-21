c58b651076e48e5502847caee4ce475622d3884b
Sebkd <sebkd@mail.ru>
2022-01-22 02:11:37 +0300

    for pull request C2L2

--- authapp/pipeline.py
+++ authapp/pipeline.py
@@ -16,22 +16,22 @@ def save_user_profile(backend, user, response, *args, **kwargs):
     '''https://api.vk.com/method/METHOD?PARAMS&access_token=TOKEN&v=V'''
     '''https://api.vk.com/method/users.get?user_ids=210700286&
     fields=bdate&access_token=533bacf01e11f55b536a565b57531ac114461ae8736d6506a3&v=5.131'''
-    api_url = urlunparse(('https',
-                          'api.vk.com',
-                          '/method/users.get',
-                          None,
-                          urlencode(OrderedDict(fields=','.join((
-                              'bdate',
-                              'sex',
-                              'about',
-                              'photo_100',
-                          )),
-                          access_token=response['access_token'],
-                          v='5.131')),
-                          None,
-                          ))
+    api_url = urlunparse (('https',
+                           'api.vk.com',
+                           '/method/users.get',
+                           None,
+                           urlencode (OrderedDict (fields = ','.join ((
+                               'bdate',
+                               'sex',
+                               'about',
+                               'photo_100',
+                           )),
+                               access_token = response['access_token'],
+                               v = '5.131')),
+                           None,
+                           ))
 
-    resp = requests.get(api_url)
+    resp = requests.get (api_url)
 
     if resp.status_code != 200:
         return
@@ -47,7 +47,7 @@ def save_user_profile(backend, user, response, *args, **kwargs):
   ]
 }
     '''
-    data = resp.json()['response'][0]
+    data = resp.json ()['response'][0]
     if data['sex']:
         user.shopuserprofile.gender = ShopUserProfile.MALE if data['sex'] == 2 \
             else ShopUserProfile.FEMALE
@@ -55,5 +55,4 @@ def save_user_profile(backend, user, response, *args, **kwargs):
     if data['about']:
         user.shopuserprofile.about_me = data['about']
 
-
-    user.save()
+    user.save ()
