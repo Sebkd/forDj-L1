d0e24177072627561d8ea8f42d7e217762ec4768
Sebkd <sebkd@mail.ru>
2022-02-06 16:44:19 +0300

    recode debug

--- myShop/tmp/settings_prodaction.py
+++ myShop/tmp/settings_prodaction.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+# DEBUG = env('DEBUG')
+DEBUG = os.environ.get("DEBUG", False) == 'True'
 
 # ALLOWED_HOSTS = ['127.0.0.1']
 
