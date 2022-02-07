44ab784669d112b015440572f5b6c706794826ef
Sebkd <sebkd@mail.ru>
2022-02-07 21:56:04 +0300

    add When Case

--- mainapp/management/commands/learn_db.py
+++ mainapp/management/commands/learn_db.py
@@ -3,7 +3,9 @@ from datetime import timedelta
 from django.core.management.base import BaseCommand
 
 # скидки
-from django.db.models import Q, F
+from django.db.models import Q, F, When, Case, IntegerField, DecimalField
+
+from orderapp.models import OrderItem
 
 
 class Command(BaseCommand):
@@ -17,13 +19,43 @@ class Command(BaseCommand):
 
         action_1__discount = 0.3
         action_2__discount = 0.15
-        action_expired = 0.05
+        action_expired__discount = 0.05
 
         # Product.objects.filter(Q(category_id=1) || Q(category_id=2)) нижнее это примерно это
         # если нет _id=1 то выводится _id=2, а если есть то и 1 и 2
-        action_1__condition = Q(order__updated__lte=F('order__created') + action_1__time_delta)
+        action_1__condition = Q(order__update__lte=F('order__created') + action_1__time_delta)
 
-        action_2__condition = Q(order__updated__gt=F('order__created') + action_1__time_delta) & \
-                                Q(order_updated__lte=F('order__created') + action_2__time_delta)
+        action_2__condition = Q(order__update__gt=F('order__created') + action_1__time_delta) & \
+                                Q(order__update__lte=F('order__created') + action_2__time_delta)
 
-        action_expired__condition = Q(order__updated__gt=F('order__created') + action_2__time_delta)
+        action_expired__condition = Q(order__update__gt=F('order__created') + action_2__time_delta)
+
+        # When возвращает True
+        action_1__order = When(action_1__condition, then=ACTION_1)
+        action_2__order = When(action_2__condition, then=ACTION_2)
+        action_expired__order = When(action_expired__condition, then=ACTION_EXPIRED)
+
+        action_1__price = When(action_1__condition, then=F('product__price') * F('quantity') * action_1__discount)
+        action_2__price = When(action_2__condition, then=F('product__price') * F('quantity') * action_2__discount)
+        action_expired__price = When(action_expired__condition,
+                                 then=F('product__price') * F('quantity') * action_expired__discount)
+
+        test_orders = OrderItem.objects.annotate(
+            action_order=Case(
+                action_1__order,
+                action_2__order,
+                action_expired__order,
+                output_field=IntegerField(),
+            )).annotate(
+            total_price=Case(
+                action_1__price,
+                action_2__price,
+                action_expired__price,
+                output_field=DecimalField(),
+        )).order_by('action_order', 'total_price').select_related()
+
+        for orderitem in test_orders:
+            print(f'{orderitem.action_order:2}: заказ №{orderitem.pk:3}:'
+                  f'{orderitem.product.name:20}: скидка '
+                  f'{abs(orderitem.total_price):6.2f} руб. | '
+                  f'{orderitem.order.update - orderitem.order.created}')
