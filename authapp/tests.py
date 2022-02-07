8746b9aa465316eda5dcb4d6a2f09956422f437d
Sebkd <sebkd@mail.ru>
2022-02-08 00:32:33 +0300

    add test register

--- authapp/tests.py
+++ authapp/tests.py
@@ -3,6 +3,8 @@ from django.test.client import Client
 from authapp.models import ShopUser
 from django.core.management import call_command
 
+from myShop import settings
+
 
 class TestUserManagement(TestCase):
 
@@ -79,6 +81,57 @@ class TestUserManagement(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.context['user'].is_anonymous)
 
+    def test_user_register(self):
+
+        # логин без данных пользователя
+        response = self.client.get('/auth/register/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['title'], 'регистрация')
+        self.assertTrue(response.context['user'].is_anonymous)
+
+        new_user_data = {
+            'username': 'samuel',
+            'first_name': 'Сэмюэл',
+            'last_name': 'Джексон',
+            'password1': 'geekbrains',
+            'password2': 'geekbrains',
+            'email': 'sumuel@geekshop.local',
+            'age': '21'}
+
+        response = self.client.post('/auth/register/', data=new_user_data)
+        self.assertEqual(response.status_code, 302)
+
+        new_user = ShopUser.objects.get(username=new_user_data['username'])
+        activation_url = f"{settings.DOMAIN_NAME}/auth/verify/{new_user_data['email']}/{new_user.activation_key} / "
+        response = self.client.get(activation_url)
+        self.assertEqual(response.status_code, 200)
+
+        # данные нового пользователя
+        self.client.login(username=new_user_data['username'],
+                          password=new_user_data['password1'])
+
+        # логинимся
+        response = self.client.get('/auth/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['user'].is_anonymous)
+
+        # проверяем главную страницу
+        response = self.client.get('/')
+        self.assertContains(response, text=new_user_data['first_name'], status_code=200)
+
+    def test_user_wrong_register(self):
+        new_user_data = {
+            'username': 'teen',
+            'first_name': 'Мэри',
+            'last_name': 'Поппинс',
+            'password1': 'geekbrains',
+            'password2': 'geekbrains',
+            'email': 'merypoppins@geekshop.local',
+            'age': '17'}
+
+        response = self.client.post('/auth/register/', data=new_user_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'register_form', 'age', 'Молод для нас! Стань старше!')
 
     def tearDown(self):
         call_command('sqlsequencereset', 'mainapp', 'authapp', 'orderapp', 'basketapp')
